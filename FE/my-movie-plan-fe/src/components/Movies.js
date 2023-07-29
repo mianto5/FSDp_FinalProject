2e7b60844ebfdfb730291d203e55ebc50340b6a8
mianto5 <tomanovamichaela5@gmail.com>
2023-07-29 08:04:47 +0200

    Searching feature.

--- FE/my-movie-plan-fe/src/components/Movies.js
+++ FE/my-movie-plan-fe/src/components/Movies.js
@@ -6,20 +6,21 @@ export default function Movies() {
   let name = sessionStorage.getItem("name");
   let role = sessionStorage.getItem("role");
   const [movies, setMovies] = useState([]);
+  const [movieSearch, setMovieSearch] = useState({ search: "" });
 
   useEffect(() => {
-    if(role === "admin"){
+    if (role === "admin") {
       fetch("http://localhost:8080/movies/all")
-      .then((response) => response.json())
-      .then((result) => {
-        setMovies(result);
-      });
-    }else{
+        .then((response) => response.json())
+        .then((result) => {
+          setMovies(result);
+        });
+    } else {
       fetch("http://localhost:8080/movies/available")
-      .then((response) => response.json())
-      .then((result) => {
-        setMovies(result);
-      });
+        .then((response) => response.json())
+        .then((result) => {
+          setMovies(result);
+        });
     }
   }, []);
 
@@ -27,16 +28,44 @@ export default function Movies() {
     <div className="container px-4 px-lg-5">
       <div className="row gx-4 gx-lg-5 align-items-center my-5">
         <h3>Available Movies</h3>
-        {(role === "admin") && (
-        <div className="text-center">
-          <NavLink className="btn btn-sm"
-              style={{backgroundColor: "#C20605", color: "white"}}
-              to={`add`}><b>ADD NEW MOVIE, GENRE OR LANGUAGE</b></NavLink>
-        </div>
+        {role === "admin" && (
+          <div className="text-center">
+            <NavLink
+              className="btn btn-sm"
+              style={{ backgroundColor: "#C20605", color: "white" }}
+              to={`add`}
+            >
+              <b>ADD NEW MOVIE, GENRE OR LANGUAGE</b>
+            </NavLink>
+          </div>
         )}
+        <div>
+          <form>
+            <div className="col-sm-12 col-md-6">
+              <label className="form-label">
+                <h5>Search Movies:</h5>
+              </label>{" "}
+              <input
+                type="text"
+                className="form-control"
+                name="search"
+                value={movieSearch.search}
+                onChange={(e) => [
+                  setMovieSearch({
+                    [e.target.name]: e.target.value,
+                  }),
+                  console.log(movieSearch),
+                ]}
+              />
+            </div>
+          </form>
+          <p>Sort:</p>
+          <p>Filter:</p>
+        </div>
         <div className="row gx-4 gx-lg-5 row-cols-1 row-cols-md-1 row-cols-lg-2 justify-content-center">
           {movies.map((movie) => (
-            <Movie key={movie.mID} movie={movie} />
+            movie.moviename.toLowerCase().includes(movieSearch.search.toLowerCase()) && (
+            <Movie key={movie.mID} movie={movie} /> )
           ))}
         </div>
       </div>
